3b06888f156a95374f4a69e22f74bca819edffda
Junyoung Son <junyoungson808@gmail.com>
2022-06-28 12:45:00 -0700

    fix button

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let myName = prompt("what is your name?")
-document.write("Nice to meet you, " + myName);
+document.write("Nice to meet you, " + myName );
 
-let doGundam = prompt("Do you like Gundam?")
+let doGundam = prompt("Do you like Coding?")
 if (doGundam === "yes") {
     console.log("YES") 
-    document.write(" LETS GOOO")
+    document.write(" I do to! ");
 }
 else {
     console.log("ITS A TRAP")
- document.write(" GUNDAAAAAAM") }
+ document.write(" keep practicing! "); }
 
