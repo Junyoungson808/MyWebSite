bb81af08693bb59faa9e87b753abbb5e6d310cc9
Junyoung Son <junyoungson808@gmail.com>
2022-06-27 23:34:29 -0700

    Javascript.

---
+++ app.js
@@ -0,0 +1,11 @@
+let myName = prompt("what is your name?")
+document.write("Nice to meet you, " + myName);
+
+let doGundam = prompt("Do you like Gundam?")
+if (doGundam === "yes") {
+    console.log("YES") 
+    document.write(" LETS GOOO")
+}
+else {
+    console.log("ITS A TRAP")
+ document.write(" GUNDAAAAAAM") }
